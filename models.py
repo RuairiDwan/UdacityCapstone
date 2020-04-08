8542dd88fd59330ee849ae555daa57a3e9f99f8c
Ruairi Dwan <ruairidwan@Ruairis-MacBook-Pro.local>
2020-04-08 14:34:23 +0100

    first commit

--- models.py
+++ models.py
@@ -1,1 +1,82 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
 
+database_name = "capstone"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+# os.environ('DATABASE_URL')
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+# Many to many relationship between movie and actor
+cast = db.Table('cast',
+    db.Column('actor_id', db.Integer, db.ForeignKey('actors.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True)
+)
+
+# Actor
+class Actors(db.Model):  
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(String)
+    gender = Column(String)
+
+    artists = db.relationship("Show", back_populates="show_venue")
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.answer = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+        'id': self.id,
+        'name': self.name,
+        'answer': self.answer,
+        'gender': self.gender
+        }
+
+# Movies
+
+class Movies(db.Model):  
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def format(self):
+        return {
+        'id': self.id,
+        'title': self.title,
+        'release_date': self.release_date
+        }
--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
 
+database_name = "capstone"
+'SQLALCHEMY_DATABASE_URI' = "postgres://{}/{}".format('localhost:5432', database_name)

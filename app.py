c19bc0fbe1522fb61d0aea572bdc098557979a53
Ruairi Dwan <ruairidwan@Ruairis-MacBook-Pro.local>
2020-04-08 16:04:10 +0100

    first commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
   setup_db(app)
   CORS(app)
 
-  @app.route('/',)
+  @app.route('/')
   def healthy_check():
     return jsonify({
       'healthy': 'check'
@@ -21,4 +21,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='rory-dwan-capstone.herokuapp.com', port=8080, debug=True)

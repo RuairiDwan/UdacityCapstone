93c4f643694d311422bee4a2fdb47c6fcdeccc5b
Ruairi Dwan <ruairidwan@Ruairis-MacBook-Pro.local>
2020-04-12 21:28:19 +0100

    Fourth Commit

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import json
 #database_name = "capstone"
 #database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
-database_path = os.environ('DATABASE_URL')
+database_path = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy()
 
